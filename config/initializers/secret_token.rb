cb3a5fc351446702e0e67feada7e27340b80af52
Navid Mostafavi <navidm@gmail.com>
2013-10-09 00:57:22 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsScratch::Application.config.secret_token = '2449f254b9e5a4af1a01f2d1ac1a1c7dae7e802f18918204983cd4ea8f1bf037f68ba6d20122511f87b236c74069b83cb8e7a471892327f042ff91d68e7e2587'
